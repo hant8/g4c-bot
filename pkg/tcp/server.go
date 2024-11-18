4b774b36daa2ee168f116c3a901bbca18e7b5365
Роман Князев <r.knyazev@get4click.com>
2024-11-18 17:23:56 +0300

    refactor(worker): Оптимизация tcp-сервера, реализация паттерна Fan-out

--- pkg/tcp/server.go
+++ pkg/tcp/server.go
@@ -7,11 +7,14 @@ import (
 	"net"
 )
 
+const workerCount int = 4
+
 func StartServer(port string) {
 	listener, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		panic(err)
 	}
+
 	defer func(listener net.Listener) {
 		err := listener.Close()
 		if err != nil {
@@ -20,6 +23,7 @@ func StartServer(port string) {
 	}(listener)
 
 	fmt.Printf("Сервер слушает на порту %s\n", port)
+	jobs := initializeWorkerPool()
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -27,29 +31,43 @@ func StartServer(port string) {
 			continue
 		}
 
-		go handleConnection(conn)
+		jobs <- conn
 	}
 }
 
-func handleConnection(conn net.Conn) {
-	defer func(conn net.Conn) {
-		err := conn.Close()
+func initializeWorkerPool() chan<- net.Conn {
+	jobs := make(chan net.Conn, 100)
+
+	for i := 0; i < workerCount; i++ {
+		go worker(i, jobs)
+	}
+
+	return jobs
+}
+
+func worker(id int, jobs <-chan net.Conn) {
+	for conn := range jobs {
+		fmt.Printf("Воркер %d обрабатывает соединение от %s\n", id, conn.RemoteAddr())
+		_, err := conn.Write([]byte("Привет, клиент!\n"))
 		if err != nil {
-			log.Fatalf("Критическая ошибка при закрытии Connection: %v", err)
+			return
 		}
-	}(conn)
 
-	fmt.Println("Новое соединение:", conn.RemoteAddr())
-	scanner := bufio.NewScanner(conn)
+		scanner := bufio.NewScanner(conn)
+		for scanner.Scan() {
+			fmt.Println("Получено:", scanner.Text())
+			_, err := conn.Write([]byte("Принято: " + scanner.Text() + "\n"))
+			if err != nil {
+				fmt.Println("Ошибка записи по соединеннию:", conn.RemoteAddr())
+				return
+			}
+		}
 
-	for scanner.Scan() {
-		fmt.Println("Получено:", scanner.Text())
-		_, err := conn.Write([]byte("Принято: " + scanner.Text() + "\n"))
+		fmt.Println("Закрытие соединения", conn.RemoteAddr())
+
+		err = conn.Close()
 		if err != nil {
-			fmt.Println("Ошибка записи по соединеннию:", conn.RemoteAddr())
 			return
 		}
 	}
-
-	fmt.Println("Закрытие соединения", conn.RemoteAddr())
 }
